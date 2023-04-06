89f7e257bbf28e2ea784b5d690f146cef1078c11
Rushabh Babre <babrerushabh1@gmail.com>
2023-04-07 00:04:32 +0530

    A lot of Debug later

--- taskmanager/mongodb.js
+++ taskmanager/mongodb.js
@@ -1,31 +1,48 @@
 // CRUD create read update delete
-//jshint esversion: 6
+// console.log("its starting?")
 
-console.log("its starting?")
+const mongoose = require('mongoose');
 
-const mongodb = require('mongodb');
-const assert = require('assert');
-const { error } = require('console');
+const connectionURL = "mongodb://127.0.0.1:27017/test";
+const databaseName = "task-manager";
 
-const MongoClient = mongodb.MongoClient;7
-const connectionURL = 'mongodb://localhost:27017/test';
-const databaseName = 'taskmanager'
+const Schema = mongoose.Schema;
+const SchemaUsers = new Schema({
+    Name: String,
+    Age: Number,
+  });
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-    if(error){
-        return console.log('Cant connect DBS')
-    }
+const connect = () => {
 
-    const db = client.db(databaseName)
-
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 69
-    }, (error,result) => {
-        if(error) {
-            return console.log('Unable to insert user')
-        }
-
-        console.log(result.ops)
+    mongoose
+    .connect(connectionURL)
+    .then(() => {
+        console.log("Connection succesful");
     })
-})
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+};
+
+connect();
+
+db = mongoose.model(databaseName, SchemaUsers);
+
+db.create({
+    Name: "Rushabh",
+    Age: 19
+},{
+    Name: "Takshal",
+    Age: 120
+},{
+    Name: "Tanish",
+    Age: 18
+},{
+    Name: "Varun",
+    Age: 17
+},{
+    Name: "Paran",
+    Age: 21
+}
+)
