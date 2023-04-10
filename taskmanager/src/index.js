76a7dd213dd23384ce55ee3fa04d0c8c0477d8ed
Rushabh Babre <babrerushabh1@gmail.com>
2023-04-10 22:51:44 +0530

    none

--- taskmanager/src/index.js
+++ taskmanager/src/index.js
@@ -9,13 +9,13 @@ const taskRouter = require("./router/task")
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use((req,res,next) => {
-    if(req.method === "GET"){
-        res.send('GET request are disabled')
-    }else {
-        next()
-    }
-})
+// app.use((req,res,next) => {
+//     if(req.method === "GET"){
+//         res.send('GET request are disabled')
+//     }else {
+//         next()
+//     }
+// })
 
 app.use((req,res,next) => {
     res.status(503).send("The server is currently undergoing maintainance")
--- taskmanager/src/router/user.js
+++ taskmanager/src/router/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const User = require("../models/user") 
+const auth = require('../middleware/auth');
+const User = require("../models/user");
 const router = new express.Router()
 
 //Create users
@@ -26,7 +27,7 @@ router.post('/users/login',async (req,res) => {
 })
 
 //Read all Users
-router.get('/users',async (req,res) => {
+router.get('/users', auth,async (req,res) => {
     try{
         const users = await User.find({})
         res.send(users)
